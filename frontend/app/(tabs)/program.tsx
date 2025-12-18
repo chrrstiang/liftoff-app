529ca63d73ca2279ca156660b46c60a4a31c68d2
chrrstiang <garcia.chris@northeastern.edu>
2025-12-18 14:07:58 -0500

    Frontend tabs set up

---
+++ frontend/app/(tabs)/program.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ProgramPage() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>Program</Text>
+    </View>
+  );
+}

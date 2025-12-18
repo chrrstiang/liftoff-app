92d455a736201bc58b35f15e8c7cf789c3053250
chrrstiang <garcia.chris@northeastern.edu>
2025-12-18 16:55:13 -0500

    Latest backend module updates

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { SupabaseModule } from './supabase/supabase.module';
+import { ValidatorsModule } from './common/validation/validators/validators.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+    }), AuthModule, UsersModule, SupabaseModule, ValidatorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { SupabaseModule } from 'src/supabase/supabase.module';
 
+/*
+Encapsulates logic regarding the authentication and authorization of users.
+*/
 @Module({
+  imports: [UsersModule, SupabaseModule],
   controllers: [AuthController],
   providers: [AuthService],
 })

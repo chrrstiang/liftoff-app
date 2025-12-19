0cc4d1d9ec7c8277f4ac6a62780a1a30c158776b
chrrstiang <garcia.chris@northeastern.edu>
2025-12-18 19:05:05 -0500

    Update _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,19 +1,10 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { useColorScheme } from "react-native";
+import Provider from "@/components/Provider";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <Provider>
+      <Stack initialRouteName="(app)" screenOptions={{ headerShown: false }} />
+    </Provider>
   );
 }
